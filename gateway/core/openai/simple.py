88f8db193177724bdc41dfb7d93ab234e113e8d4
Amol Dumrewal <amoldumrewal@gmail.com>
2024-09-21 21:43:06 +0530

    Add simple OpenAI API call file

---
+++ gateway/core/openai/simple.py
@@ -0,0 +1,19 @@
+from openai import OpenAI, NotGiven
+
+OPENAI_API_KEY = ""
+
+
+
+client = OpenAI(api_key=OPENAI_API_KEY)
+
+def json_call(model_name, system_prompt, user_prompt, json_response=False, temperature=0):
+    response = client.chat.completions.create(
+        model=model_name,
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": user_prompt},
+        ],
+        temperature=temperature,
+        response_format={"type": "json_object"} if json_response else NotGiven,
+    )
+    return response.choices[0].message.content
